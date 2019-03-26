dce49dfbf53af7b0a1338c93b6052e62c6d638f8
Ian Ross <ian@skybluetrades.net>
2019-03-26 13:48:08 +0100

    Basic task manager app

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,42 @@
 |
 */
 
+use Illuminate\Http\Request;
+use App\Task;
+
+// Show Tasks
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+// Add New Task
+Route::post('/task', function (Request $request) {
+    // Validate input
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // Create task
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+// Delete Task
+Route::delete('/task/{task}', function (Task $task) {
+  $task->delete();
+
+  return redirect('/');
 });
